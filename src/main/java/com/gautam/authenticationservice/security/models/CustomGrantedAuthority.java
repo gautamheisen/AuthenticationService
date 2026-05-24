040f9e8cdddea2fead0bc7dae293c5a883c1d8cd
gautamheisen <gautams941@gmail.com>
2026-05-24 12:35:49 +0530

    oauth2 setup

--- src/main/java/com/gautam/authenticationservice/security/models/CustomGrantedAuthority.java
+++ src/main/java/com/gautam/authenticationservice/security/models/CustomGrantedAuthority.java
@@ -1,18 +1,27 @@
 package com.gautam.authenticationservice.security.models;
 
 import com.gautam.authenticationservice.models.Role;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 
+
+@NoArgsConstructor
 public class CustomGrantedAuthority implements GrantedAuthority {
-    private Role role;
+    //    private Role role;
+    private String authority;
 
     public CustomGrantedAuthority(Role role) {
-        this.role = role;
+        this.authority = role.getName();
     }
+
 
     @Override
     public String getAuthority() {
-        return role.getName();
+        return this.authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
     }
 }
 
--- src/main/java/com/gautam/authenticationservice/security/models/CustomUserDetails.java
+++ src/main/java/com/gautam/authenticationservice/security/models/CustomUserDetails.java
@@ -2,6 +2,7 @@ package com.gautam.authenticationservice.security.models;
 
 import com.gautam.authenticationservice.models.Role;
 import com.gautam.authenticationservice.models.User;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -9,51 +10,118 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+@NoArgsConstructor
 public class CustomUserDetails implements UserDetails {
-    private final User user;
+    private List<GrantedAuthority> authorities;
+    private String password;
+    private String username;
+    private boolean accountNonExpired;
+    private boolean accountNonLocked;
+    private boolean credentialsNonExpired;
+    private boolean enabled;
+    private Long userId;
+
+
 
     public CustomUserDetails(User user) {
-        this.user = user;
+
+//        this.user = user;
+        authorities = new ArrayList<>();
+
+        for (Role role: user.getRoles()) {
+            authorities.add(new CustomGrantedAuthority(role));
+        }
+
+        this.password = user.getPassword();
+        this.username = user.getEmail();
+        this.accountNonExpired = true;
+        this.accountNonLocked = true;
+        this.credentialsNonExpired = true;
+        this.enabled = true;
+        this.userId = user.getId();
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        List<CustomGrantedAuthority> customGrantedAuthorities = new ArrayList<>();
+//        List<CustomGrantedAuthority> customGrantedAuthorities = new ArrayList<>();
+//
+//        for (Role role: user.getRoles()) {
+//            customGrantedAuthorities.add(new CustomGrantedAuthority(role));
+//        }
+//
+//        return customGrantedAuthorities;
+        return this.authorities;
+    }
 
-        for (Role role: user.getRoles()) {
-            customGrantedAuthorities.add(new CustomGrantedAuthority(role));
-        }
-
-        return customGrantedAuthorities;
+    public void setAuthorities(List<GrantedAuthority> authorities) {
+        this.authorities = authorities;
     }
 
     @Override
     public String getPassword() {
-        return user.getPassword();
+//        return user.getPassword();
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     @Override
     public String getUsername() {
-        return user.getEmail();
+//        return user.getEmail();
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return true;
+//        return true;
+        return this.accountNonExpired;
+    }
+
+    public void setAccountNonExpired(boolean accountNonExpired) {
+        this.accountNonExpired = accountNonExpired;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return true;
+//        return true;
+        return this.accountNonLocked;
+    }
+
+    public void setAccountNonLocked(boolean accountNonLocked) {
+        this.accountNonLocked = accountNonLocked;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return true; // if (Date.now() - lastPasswordUpdateDate > 90) {return false} return true;
+//        return true; // if (Date.now() - lastPasswordUpdateDate > 90) {return false} return true;
+        return this.credentialsNonExpired;
+    }
+
+    public void setCredentialsNonExpired(boolean credentialsNonExpired) {
+        this.credentialsNonExpired = credentialsNonExpired;
     }
 
     @Override
     public boolean isEnabled() {
-        return true;
+//        return true;
+        return this.enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
     }
 }
